ae8863d686c61abae472da8e12a1fcb5104ac41b
weijian49 <112360606+weijian49@users.noreply.github.com>
2025-06-25 16:07:31 +0900

    Update FizzBuzz.java

--- src/main/java/schoo/jenkins/FizzBuzz.java
+++ src/main/java/schoo/jenkins/FizzBuzz.java
@@ -6,7 +6,6 @@ public class FizzBuzz {
             return "FizzBuzz";
         } else if (value % 3 == 0) {
             return "Fizz";
-            
         } else if (value % 5 == 0) {
             return "Buzz";
         } else {
